4fd1698a1023deea915c13ec94442dc3efe1f36e
Eric Muller <EricMuller22@gmail.com>
2013-07-14 11:59:16 -0700

    Album and blurred bg view placeholders

--- radio/AlbumViewController.m
+++ radio/AlbumViewController.m
@@ -7,10 +7,12 @@
 //
 
 #import "AlbumViewController.h"
+#import <AFNetworking/UIImageView+AFNetworking.h>
 
 @interface AlbumViewController ()
 
 @property (nonatomic, strong) UIImageView *albumView;
+@property (nonatomic, strong) UIImageView *bgView;
 
 @end
 
@@ -20,7 +22,13 @@
 {
     self = [super init];
     if (self) {
+        // album art
         _albumView = [[UIImageView alloc] initWithFrame:CGRectMake(60,120,200,200)];
+        
+        // blurred album art placeholder
+        _bgView = [[UIImageView alloc] initWithFrame:self.view.frame];
+        
+        [self.view addSubview:_bgView];
         [self.view addSubview:_albumView];
     }
     return self;
@@ -28,7 +36,8 @@
 
 - (void)viewWillAppear:(BOOL)animate
 {
-    // self.albumView.image = TODO
+    NSURL *url = [NSURL URLWithString:@"http://ecx.images-amazon.com/images/I/917Z407djoL._SL1500_.jpg"];
+    [self.albumView setImageWithURL:url];
 }
 
 @end
