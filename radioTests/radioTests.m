8ee40eeb16294d1af79d749fa8e6987b76c7adfd
Eric Muller <EricMuller22@gmail.com>
2013-07-13 23:57:01 -0700

    Initial Commit

---
+++ radioTests/radioTests.m
@@ -0,0 +1,36 @@
+//
+//  radioTests.m
+//  radioTests
+//
+//  Created by Eric Muller on 7/13/13.
+//  Copyright (c) 2013 Unexplored Novelty, LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface radioTests : XCTestCase
+
+@end
+
+@implementation radioTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
