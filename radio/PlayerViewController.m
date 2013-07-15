e7f4b3551a327d323010eec4b3e21ac2fd532ba5
Eric Muller <EricMuller22@gmail.com>
2013-07-14 19:34:08 -0700

    Progress bar

--- radio/PlayerViewController.m
+++ radio/PlayerViewController.m
@@ -12,6 +12,10 @@
 @interface PlayerViewController ()
 
 @property (strong, nonatomic) UIToolbar *controls;
+@property (strong, nonatomic) UIProgressView *progress;
+@property (strong, nonatomic) UIBarButtonItem *play;
+@property (strong, nonatomic) UIBarButtonItem *back;
+@property (strong, nonatomic) UIBarButtonItem *next;
 
 @end
 
@@ -28,13 +32,15 @@
 
 - (void)viewDidLoad
 {
+    // playback controls
     _controls = [[UIToolbar alloc] init];
     _controls.frame = CGRectMake(0,
                                  208,
                                  self.view.bounds.size.width,
                                  40);
-    _controls.barStyle = UIBarStyleDefault;
-    _controls.barTintColor = [UIColor colorFromRGB:0x000000 alpha:0.0f];
+    _controls.barStyle = UIBarStyleBlack;
+    _controls.barTintColor = [UIColor colorFromRGB:0x333333 alpha:1.0f];
+    _controls.translucent = NO;
     UIBarButtonItem *flexibleSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
                                                                                    target:nil
                                                                                    action:nil];
@@ -42,17 +48,25 @@
                                                                                 target:nil
                                                                                 action:nil];
     fixedSpace.width = 40;
-    UIBarButtonItem *playButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemPlay
+    _play = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemPlay
                                                                                 target:self
                                                                                 action:@selector(didPressPlay)];
-    UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRewind
+    _back = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRewind
                                                                                 target:self
                                                                                 action:@selector(didPressBack)];
-    UIBarButtonItem *nextButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFastForward
+    _next = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFastForward
                                                                                 target:self
                                                                                 action:@selector(didPressNext)];
-    [_controls setItems:@[flexibleSpace, backButton, fixedSpace, playButton, fixedSpace, nextButton, flexibleSpace]];
+    [_controls setItems:@[flexibleSpace, _back, fixedSpace, _play, fixedSpace, _next, flexibleSpace]];
     [self.view addSubview:_controls];
+    
+    // playback progress
+    _progress = [[UIProgressView alloc] initWithProgressViewStyle:UIProgressViewStyleBar];
+    _progress.frame = CGRectMake(0, 206, 320, 2);
+    _progress.progressTintColor = [UIColor whiteColor];
+    _progress.trackTintColor = [UIColor clearColor];
+    _progress.progress = 0.4f;
+    [self.view addSubview:_progress];
 }
 
 - (void)didPressPlay
